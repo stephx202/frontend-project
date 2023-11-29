7b02e2d2bcf703832d1f1a865bfff345ba54ea93
stephx202 <stephhernandez202@gmail.com>
2023-11-29 14:06:25 -0800

    update 7: displays products data based on user input

--- app.js
+++ app.js
@@ -19,15 +19,29 @@ $(document).ready(function(){
     //function to call inside the brandform event listener when called
     function displayBrandProducts(products){
         //clear the products that were already displayed
-        $productsList.empty();
-        //if brand's products is unavailable, return a text
+        $productsList.empty(); 
+        //if input brand  is unavailable, return a text
         if(products.length===0){
-            $productsList.text("We are currently out of stock for this brand. Please try again later :(")
-        //otherwise, append to the products list
+            $productsList.text("We are currently out of stock. Please try again later :(")
+        //otherwise, append to the products list. attach
         }else{
             products.forEach((product) =>{
+                let $productContainer = $("<div></div>").addClass("productInfo")
+                let $productImage = $("<img></img>").attr("src", product.api_featured_image);
+                let $productBrand = $("<div></div>").text(product.brand)
+                let $productCategory = $("<div></div>").text(product.category);
+                let $productName = $("<div></div>").text(product.name);
+                let $productPrice = $("<div></div>").text("$" + product.price);
+                let $productType = $("<div></div>").text(product.product_type)
+                $productContainer.append($productBrand);
+                $productContainer.append($productCategory);
+                $productContainer.append($productName);
+                $productContainer.append($productPrice);
+                $productContainer.append($productType);
+                $productContainer.append($productImage)
+                $productsList.append($productContainer)
                 
-            })
+            });
         }
     }
 
