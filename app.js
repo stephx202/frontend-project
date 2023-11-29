628ddfce11880fee7c7cf56c7be60e0a72e36e38
stephx202 <stephhernandez202@gmail.com>
2023-11-29 12:49:48 -0800

    update 6: if bran unavailable, returns try again later

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 $(document).ready(function(){
+    //want to append to the productsList. This is going to be the results.
+    let $productsList = $("#productsList");
+
+    //event listener for user's brand input when button clicked and to stop prior to submission.
+    $("#buttonID").on("click",function(e){
+        e.preventDefault()
+        //encodeURI
+        let brandInput = $("#brandInput").val();
+        let brandInputURI = encodeURI(brandInput);
+        //get API and attach brandInputURI at the end to retrieve data for specific brand.
+        $.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandInputURI}`,(data) => {
+            console.log(data);
+            //call function for when user searches another brand
+            displayBrandProducts(data)
+
+        })
+    });
+    //function to call inside the brandform event listener when called
+    function displayBrandProducts(products){
+        //clear the products that were already displayed
+        $productsList.empty();
+        //if brand's products is unavailable, return a text
+        if(products.length===0){
+            $productsList.text("We are currently out of stock for this brand. Please try again later :(")
+        //otherwise, append to the products list
+        }else{
+            products.forEach((product) =>{
+                
+            })
+        }
+    }
 
 
 
 
-//want to append to the productsList. This is going to be the results.
-//event listener for brand form submit and to stop prior to submission.
-// $("#brandForm").on("submit",function(e){
-//     e.preventDefault()
-//     let brandInput = $("#brandInput").val();
-//     let brandInputURI = encodeURI(brandInput);
-// })
-// //create brandName with value of user's input (brandInput).
-// $.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline", (data) => {
-// console.log(data);
-// });
-//debugger;
-const button= $("#buttonID")
-button.on("click",function(e){
-    console.log("hit")
-     e.preventDefault()
-//     let brandInput = $("#brandInput").val();
-//     let brandInputURI = encodeURI(brandInput);
-//     //debugger;
-//     $.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandInputURI}`,(data) => {
-// console.log(data);
-// })
-// //create brandName with value of user's input (brandInput).
- });
-})
+
+
+
+//last bracket
+});
 
