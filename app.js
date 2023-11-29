54470bc897e7729b3ea97fcc739ed282ea5999c9
stephx202 <stephhernandez202@gmail.com>
2023-11-29 10:21:46 -0800

    update 4: got data from hardcoding a brand in $.get

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //want to append to the productsList. This is going to be the results.
 //event listener for brand form submit and to stop prior to submission.
-$("#brandForm").submit(function(e){
+$("#brandForm").on("submit",function(e){
     e.preventDefault()
 })
 //create brandName with value of user's input (brandInput).
-let brandName = $("#brandInput").val();
-$.get("http://makeup-api.herokuapp.com/api/v1/products.json", {brand: brandName }, (data) => {
-    console.log(data);
+let brandInput = $("#brandInput").val();
+$.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline", (data) => {
+console.log(data);
 });
