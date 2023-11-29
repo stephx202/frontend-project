e596f4f2a61304476089cf2f2ad5c05d0dda4c10
stephx202 <stephhernandez202@gmail.com>
2023-11-29 11:57:48 -0800

    update 5: button even listener works

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
+$(document).ready(function(){
+
+
+
+
 //want to append to the productsList. This is going to be the results.
 //event listener for brand form submit and to stop prior to submission.
-$("#brandForm").on("submit",function(e){
-    e.preventDefault()
+// $("#brandForm").on("submit",function(e){
+//     e.preventDefault()
+//     let brandInput = $("#brandInput").val();
+//     let brandInputURI = encodeURI(brandInput);
+// })
+// //create brandName with value of user's input (brandInput).
+// $.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline", (data) => {
+// console.log(data);
+// });
+//debugger;
+const button= $("#buttonID")
+button.on("click",function(e){
+    console.log("hit")
+     e.preventDefault()
+//     let brandInput = $("#brandInput").val();
+//     let brandInputURI = encodeURI(brandInput);
+//     //debugger;
+//     $.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandInputURI}`,(data) => {
+// console.log(data);
+// })
+// //create brandName with value of user's input (brandInput).
+ });
 })
-//create brandName with value of user's input (brandInput).
-let brandInput = $("#brandInput").val();
-$.get("http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline", (data) => {
-console.log(data);
-});
+
