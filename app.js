db64ea85ce86e09cf9bf45497860ccf14eeff9cf
stephx202 <stephhernandez202@gmail.com>
2023-11-29 20:57:09 -0800

    update 7: select the search type works

--- app.js
+++ app.js
@@ -1,56 +1,63 @@
 $(document).ready(function(){
-    //want to append to the productsList. This is going to be the results.
+    // Want to append to the productsList. This is going to be the results.
     let $productsList = $("#productsList");
 
-    //event listener for user's brand input when button clicked and to stop prior to submission.
-    $("#buttonID").on("click",function(e){
-        e.preventDefault()
-        //encodeURI
-        let brandInput = $("#brandInput").val();
-        let brandInputURI = encodeURI(brandInput);
-        //get API and attach brandInputURI at the end to retrieve data for specific brand.
-        $.get(`http://makeup-api.herokuapp.com/api/v1/products.json?brand=${brandInputURI}`,(data) => {
-            console.log(data);
-            //call function for when user searches another brand
-            displayBrandProducts(data)
+    // Event listener for user's brand input when the button is clicked and to stop prior to submission.
+    $("#buttonID").on("click", function(e){
+        e.preventDefault();
 
-        })
+        // Encode URI with the user's search input value. create variable with set value to search type.
+        let searchType = $("#searchType").val();
+        let searchInput = $("#searchInput").val();
+        let searchInputURI = encodeURI(searchInput);
+
+        // Get API and attach brandInputURI at the end to retrieve data for a specific brand.
+        //create api's url variable to reassign later. (when appending searchInputURI)
+        let url = "";
+
+        //if the search type value is what user selected, then reassign api's url to that specific one
+        //and add the search input URI at the end of the value for new url.
+        if (searchType === "brand") {
+            url = `http://makeup-api.herokuapp.com/api/v1/products.json?brand=${searchInputURI}`;
+        } else if (searchType === "product_type") {
+            url = `http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${searchInputURI}`;
+        }
+        //////////////////******ADD MORE URLS LIKE WHATS ABOVE************* *////////////////////////
+        
+        //use $.get and attach url for search type with search input, and display data (console.log(data)).
+        $.get(url, (data) => {
+            console.log(data);
+            // Call function for when the user clicks search button. display elements on each product. 
+            displayProducts(data);
+        });
     });
-    //function to call inside the brandform event listener when called
-    function displayBrandProducts(products){
-        //clear the products that were already displayed
-        $productsList.empty(); 
-        //if input brand  is unavailable, return a text
-        if(products.length===0){
-            $productsList.text("We are currently out of stock. Please try again later :(")
-        //otherwise, append to the products list. attach
-        }else{
-            products.forEach((product) =>{
-                let $productContainer = $("<div></div>").addClass("productInfo")
-                let $productImage = $("<img></img>").attr("src", product.api_featured_image);
-                let $productBrand = $("<div></div>").text(product.brand)
-                let $productCategory = $("<div></div>").text(product.category);
-                let $productName = $("<div></div>").text(product.name);
-                let $productPrice = $("<div></div>").text("$" + product.price);
-                let $productType = $("<div></div>").text(product.product_type)
-                $productContainer.append($productBrand);
-                $productContainer.append($productCategory);
-                $productContainer.append($productName);
-                $productContainer.append($productPrice);
-                $productContainer.append($productType);
-                $productContainer.append($productImage)
-                $productsList.append($productContainer)
-                
+
+    // Function to call inside the event listener when click search button. displays each product with its elements
+    function displayProducts(products) {
+        // Clear the products that were already displayed
+        $productsList.empty();
+
+        // If input is unavailable, return "currently out of stock".
+        if (products.length === 0) {
+            $productsList.text("We are currently out of stock. Please try again later :(");
+        // Otherwise, append each element to the products list.
+        } else {
+            products.forEach((product) => {
+                let productContainer = $("<div></div>").addClass("productInfo");
+                let productImage = $("<img></img>").attr("src", product.api_featured_image).css("width","150px").css("height","auto");
+                let productBrand = $("<div></div>").text(product.brand);
+                let productCategory = $("<div></div>").text(product.category);
+                let productName = $("<div></div>").text(product.name);
+                let productPrice = $("<div></div>").text("$" + product.price);
+                let productType = $("<div></div>").text(product.product_type);
+                productContainer.append(productImage);
+                productContainer.append(productBrand);
+                productContainer.append(productCategory);
+                productContainer.append(productName);
+                productContainer.append(productPrice);
+                productContainer.append(productType);
+                $productsList.append(productContainer);
             });
         }
     }
-
-
-
-
-
-
-
-//last bracket
 });
-
